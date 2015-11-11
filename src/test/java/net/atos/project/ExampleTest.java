c809b5ee636475c10e35a8099ceef7c6a3f924dc
john gordon <John1@johns-MBP.home>
2015-11-11 20:14:43 +0000

    Maven changes

---
+++ src/test/java/net/atos/project/ExampleTest.java
@@ -0,0 +1,14 @@
+package net.atos.project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		assertEquals(2, 2);
+	}
+
+}
